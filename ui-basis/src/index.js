8320d8cdaf6562b46a868c33276523f470a326bd
Fernanda Ferry <nandateixferry@gmail.com>
2020-04-19 01:49:47 -0300

    Itens da tela de casdastro de loja.

---
+++ ui-basis/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+class Input extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            nome: "",
+            cnpj: "",
+            cep: "",
+            numero: ""
+        };
+        this.onChange = (evento) => {
+            const  state = Object.assign({}, this.state);
+            const campo = evento.target.name;
+            state[campo] = evento.target.value;
+            this.setState(state);
+        };
+        this.onSubmit = (evento) => {
+            evento.preventDefault();
+            console.log(this.state);
+        };
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Cadastro</h2>
+                Nome da loja:  <input name="nome" value={this.state.nome} onChange={this.onChange} type="text"/> < br />
+                CNPJ: <input name="cnpj" value={this.state.cnpj} onChange={this.onChange} type="text"/> < br />
+                Razão Social:<input type="text" disabled /> <br />
+                CEP:<input name="cep" value={this.state.cep} onChange={this.onChange} type="text"/> < br />
+                Rua: <input type="text" disabled /> <br />
+                Bairro: <input type="text" disabled /> <br />
+                Complemento: <input type="text" disabled /> <br />
+                Cidade: <input type="text" disabled /> <br />
+                Estado: <input type="text" disabled /> <br />
+                Número: <input name="numero" value={this.state.numero} onChange={this.onChange} type="text"/>
+                <br />
+                <button onClick={this.onSubmit}>enviar</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+  <Input />,
+  document.getElementById('root')
+);
+

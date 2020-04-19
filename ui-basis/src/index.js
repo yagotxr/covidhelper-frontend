1eb82ebb340628537bde1d9e1aa63c2f7dd13949
Fernanda Ferry <nandateixferry@gmail.com>
2020-04-19 02:43:58 -0300

    Bootstrap tela de cadastro de loja

--- ui-basis/src/index.js
+++ ui-basis/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, Col} from "react-bootstrap";
 
 class Input extends React.Component{
     constructor() {
@@ -11,7 +11,8 @@ class Input extends React.Component{
             nome: "",
             cnpj: "",
             cep: "",
-            numero: ""
+            numero: "",
+            complemento: ""
         };
         this.onChange = (evento) => {
             const  state = Object.assign({}, this.state);
@@ -26,52 +27,85 @@ class Input extends React.Component{
     }
 
     render(){
-        return(<div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                <div>
-                    <h2>Cadastro</h2>
-                    Nome da loja:  <input name="nome" value={this.state.nome} onChange={this.onChange} type="text"/> < br />
-                    CNPJ: <input name="cnpj" value={this.state.cnpj} onChange={this.onChange} type="text"/> < br />
-                    Razão Social:<input type="text" disabled /> <br />
-                    CEP:<input name="cep" value={this.state.cep} onChange={this.onChange} type="text"/> < br />
-                    Rua: <input type="text" disabled /> <br />
-                    Bairro: <input type="text" disabled /> <br />
-                    Complemento: <input type="text" disabled /> <br />
-                    Cidade: <input type="text" disabled /> <br />
-                    Estado: <input type="text" disabled /> <br />
-                    Número: <input name="numero" value={this.state.numero} onChange={this.onChange} type="text"/>
+        return(<div className={"formulario"}>
+                <Form>
+                    <h2>Cadastro de loja</h2>
                     <br />
-                    <button onClick={this.onSubmit}>enviar</button>
-                </div>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email"/>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="senha">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password"/>
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="cnpj">
+                            <Form.Label>CNPJ</Form.Label>
+                            <Form.Control name="cnpj" value={this.state.cnpj} onChange={this.onChange}  />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="razao_social">
+                            <Form.Label>Razão Social</Form.Label>
+                            <Form.Control name="nome" value={this.state.nome} onChange={this.onChange} />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group controlId="cep">
+                            <Form.Label>CEP</Form.Label>
+                            <Form.Control  name="cep" value={this.state.cep} onChange={this.onChange} />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="rua">
+                            <Form.Label>Rua</Form.Label>
+                            <Form.Control disabled />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="cidade">
+                            <Form.Label>Cidade</Form.Label>
+                            <Form.Control disabled />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="estado">
+                            <Form.Label>Estado</Form.Label>
+                            <Form.Control as="select" value="Choose...">
+                                <option>Mg</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="bairro">
+                            <Form.Label>Bairro</Form.Label>
+                            <Form.Control  disabled/>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="numero">
+                            <Form.Label>Numero</Form.Label>
+                            <Form.Control  name="numero" value={this.state.numero} onChange={this.onChange}></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="complemento">
+                            <Form.Label>Complemento</Form.Label>
+                            <Form.Control name="complemento" value={this.state.complemento} onChange={this.onChange}></Form.Control>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Button variant="primary" onSubmit={this.state} type="submit">
+                        Enviar
+                    </Button>
+                </Form>
+
             </div>
         );
     }
 }
 
 ReactDOM.render(
-  <Input />,
+    <Input />,
+
   document.getElementById('root')
 );
 

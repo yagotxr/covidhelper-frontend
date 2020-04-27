683af18861619bd164dc3f86bf2efafd7843cb39
Fernanda Ferry <nandateixferry@gmail.com>
2020-04-26 21:47:50 -0300

    Adicionado formulario de CPF e escolha de cpf/cnpj

--- ui-basis/src/index.js
+++ ui-basis/src/index.js
@@ -6,12 +6,14 @@ import {FormGroup, Label, Input, Button} from "reactstrap";
 import "./assets/css/bootstrap.min.css";
 import "./assets/css/paper-kit.css";
 
+
 class InputC extends React.Component{
     constructor() {
         super();
         this.state = {
             nome: "",
             cnpj: "",
+            cpf:"",
             cep: "",
             numero: "",
             complemento: "",
@@ -27,6 +29,14 @@ class InputC extends React.Component{
             evento.preventDefault();
             console.log(this.state);
         };
+        this.onClick = (id) =>{
+            var resultDiv = document.getElementById(id).style.display;
+            if(resultDiv === "none"){
+                document.getElementById(id).style.display = 'block';
+            } else{
+                document.getElementById(id).style.display = 'none';
+            }
+        }
     }
 
     render(){
@@ -34,52 +44,107 @@ class InputC extends React.Component{
             <div className={"formulario"}>
                 <h2 className={"login_texto"}>Login</h2>
                 <form>
-                    <div className="form-row">
+                    <div className="form-row" id="radio">
                         <FormGroup className="col-md-6">
-                            <Label for="inputCNPJ">CNPJ</Label>
-                            <Input  id="inputCNPJ" value={this.state.cnpj} onChange={this.onChange} />
-                        </FormGroup>
-                        <FormGroup className="col-md-6">
-                            <Label for="inputRazao">Razão Social</Label>
-                            <Input type="text"  id="inputRazao" disabled />
+                           <Input type="radio" id="tipo_id" name="tipo" value="cnpj"/> CNPJ<br/>
+                           <Input type="radio" id="tipo_id" name="tipo" value="cpf"/> CPF
                         </FormGroup>
                     </div>
-                    <div className="form-row">
-                        <FormGroup className="col-md-6">
-                            <Label for="inputZip">CEP</Label>
-                            <Input type="text"  id="inputZip" value={this.state.cep} onChange={this.onChange}/>
-                        </FormGroup>
-                        <FormGroup className="col-md-6">
-                            <Label for="inputStreet">Rua</Label>
-                            <Input type="text"  id="inputStreet" disabled/>
-                        </FormGroup>
+                    <div id='cnpj_div' style={{display:"none"}}>
+                        <div className="form-row" >
+                            <FormGroup className="col-md-6">
+                                <Label for="inputCNPJ" >CNPJ</Label>
+                                <Input  id="inputCNPJ" value={this.state.cnpj} onChange={this.onChange} />
+                            </FormGroup>
+                            <FormGroup className="col-md-6">
+                                <Label for="inputRazao">Razão Social</Label>
+                                <Input type="text"  id="inputRazao" disabled />
+                            </FormGroup>
+                        </div>
+                        <div className="form-row">
+                            <FormGroup className="col-md-6">
+                                <Label for="inputZip">CEP</Label>
+                                <Input type="text"  id="inputZip" value={this.state.cep} onChange={this.onChange}/>
+                            </FormGroup>
+                            <FormGroup className="col-md-6">
+                                <Label for="inputStreet">Rua</Label>
+                                <Input type="text"  id="inputStreet" disabled/>
+                            </FormGroup>
+                        </div>
+                        <div className="form-row">
+                            <FormGroup className="col-md-6">
+                                <Label for="inputCity">Cidade</Label>
+                                <Input type="text"  id="inputCity" disabled/>
+                            </FormGroup>
+                            <FormGroup className="col-md-2">
+                                <Label for="inputState">Estado</Label>
+                                <Input type="text" id="inputState" disabled />
+                            </FormGroup>
+                            <FormGroup className="col-md-4">
+                                <Label for="inputBairro">Bairro</Label>
+                                <Input type="text" id="inputBairro" disabled />
+                            </FormGroup>
+                            <FormGroup className="col-md-4">
+                                <Label for="inputComplement">Complemento</Label>
+                                <Input type="text" id="inputComplement" value={this.state.complemento} onChange={this.onChange}  />
+                            </FormGroup>
+                            <FormGroup className="col-md-2">
+                                <Label for="inputNumber">Numero</Label>
+                                <Input type="text" id="inputNumber" value={this.state.numero} onChange={this.onChange}  />
+                            </FormGroup>
+                            <FormGroup className="col-md-6">
+                                <Label for="inputTel">Telefone</Label>
+                                <Input type="text" id="inputTel" value={this.state.telefone} onChange={this.onChange}  />
+                            </FormGroup>
+                        </div>
                     </div>
-                    <div className="form-row">
-                        <FormGroup className="col-md-6">
-                            <Label for="inputCity">Cidade</Label>
-                            <Input type="text"  id="inputCity" disabled/>
-                        </FormGroup>
-                        <FormGroup className="col-md-2">
-                            <Label for="inputState">Estado</Label>
-                            <Input type="text" id="inputState" disabled />
-                        </FormGroup>
-                        <FormGroup className="col-md-4">
-                            <Label for="inputBairro">Bairro</Label>
-                            <Input type="text" id="inputBairro" disabled />
-                        </FormGroup>
-                        <FormGroup className="col-md-4">
-                            <Label for="inputComplement">Complemento</Label>
-                            <Input type="text" id="inputComplement" value={this.state.complemento} onChange={this.onChange}  />
-                        </FormGroup>
-                        <FormGroup className="col-md-2">
-                            <Label for="inputNumber">Numero</Label>
-                            <Input type="text" id="inputNumber" value={this.state.numero} onChange={this.onChange}  />
-                        </FormGroup>
-                        <FormGroup className="col-md-6">
-                            <Label for="inputTel">Telefone</Label>
-                            <Input type="text" id="inputTel" value={this.state.telefone} onChange={this.onChange}  />
-                        </FormGroup>
-
+                        <div id="cpf_div" style={{display:"block"}}>
+                            <div className="form-row" >
+                                <FormGroup className="col-md-6">
+                                    <Label for="inputCpf" >CPF</Label>
+                                    <Input  id="inputCpf" value={this.state.cpf} onChange={this.onChange} />
+                                </FormGroup>
+                                <FormGroup className="col-md-6">
+                                    <Label for="inputNome">Nome</Label>
+                                    <Input type="text"  id="inputNome" disabled />
+                                </FormGroup>
+                            </div>
+                            <div className="form-row">
+                                <FormGroup className="col-md-6">
+                                    <Label for="inputZip">CEP</Label>
+                                    <Input type="text"  id="inputZip" value={this.state.cep} onChange={this.onChange}/>
+                                </FormGroup>
+                                <FormGroup className="col-md-6">
+                                    <Label for="inputStreet">Rua</Label>
+                                    <Input type="text"  id="inputStreet" disabled/>
+                                </FormGroup>
+                            </div>
+                            <div className="form-row">
+                                <FormGroup className="col-md-6">
+                                    <Label for="inputCity">Cidade</Label>
+                                    <Input type="text"  id="inputCity" disabled/>
+                                </FormGroup>
+                                <FormGroup className="col-md-2">
+                                    <Label for="inputState">Estado</Label>
+                                    <Input type="text" id="inputState" disabled />
+                                </FormGroup>
+                                <FormGroup className="col-md-4">
+                                    <Label for="inputBairro">Bairro</Label>
+                                    <Input type="text" id="inputBairro" disabled />
+                                </FormGroup>
+                                <FormGroup className="col-md-4">
+                                    <Label for="inputComplement">Complemento</Label>
+                                    <Input type="text" id="inputComplement" value={this.state.complemento} onChange={this.onChange}  />
+                                </FormGroup>
+                                <FormGroup className="col-md-2">
+                                    <Label for="inputNumber">Numero</Label>
+                                    <Input type="text" id="inputNumber" value={this.state.numero} onChange={this.onChange}  />
+                                </FormGroup>
+                                <FormGroup className="col-md-6">
+                                    <Label for="inputTel">Telefone</Label>
+                                    <Input type="text" id="inputTel" value={this.state.telefone} onChange={this.onChange}  />
+                                </FormGroup>
+                            </div>
                     </div>
                     <Button type="submit" onSubmit={this.state} color="info">Entrar</Button>
                 </form>

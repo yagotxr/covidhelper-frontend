2f4c9753b27dcb80b5d11da36fa4ba5041d61bab
Fernanda Ferry <nandateixferry@gmail.com>
2020-06-24 10:53:52 -0300

    Logando a localização(lat,long)

--- ui-basis/src/components/Home/Home.jsx
+++ ui-basis/src/components/Home/Home.jsx
@@ -2,10 +2,10 @@ import React, { Component} from 'react'
 
 import '../../assets/scss/Form.scss'
 import { 
-    Card, CardBody, CardTitle, Input, CardHeader, 
-    InputGroup, InputGroupAddon, Label, InputGroupText, Button
+    Card, CardBody, CardTitle,CardHeader, Label
 } from 'reactstrap';
 import axios from 'axios';
+import Map from '../Map/Map';
 
 
 class Home extends Component {
@@ -27,61 +27,30 @@ class Home extends Component {
           .catch(error => {
               console.log(error)
           })
-      }
+    }
 
     render() {
         const { dados } = this.state
         return (
             <div>
-                <Card color="light" style={{  width: '30rem', height: '20rem'}}>
+                <Card color="light" style={{  width: '30rem', height: '17rem'}}>
                     <CardHeader >
                         <CardTitle >
                             NÚMERO DE CASOS POR ESTADO 
                         </CardTitle>
                     </CardHeader>
                     <CardBody>
-                            <br/>
-                            <InputGroup>
-                                <InputGroupAddon addonType="prepend">
-                                <InputGroupText>Estado</InputGroupText>
-                                </InputGroupAddon>
-                                    <Input type="select" md={2} select id="UF" name="UF">
-                                        <option value="">Selecione</option>
-                                        <option value="AC">AC</option>
-                                        <option value="AL">AL</option>
-                                        <option value="AP">AP</option>
-                                        <option value="AM">AM</option>
-                                        <option value="BA">BA</option>
-                                        <option value="CE">CE</option>
-                                        <option value="DF">DF</option>
-                                        <option value="ES">ES</option>
-                                        <option value="GO">GO</option>
-                                        <option value="MA">MA</option>
-                                        <option value="MS">MS</option>
-                                        <option value="MT">MT</option>
-                                        <option value="MG">MG</option>
-                                        <option value="PA">PA</option>
-                                        <option value="PB">PB</option>
-                                        <option value="PR">PR</option>
-                                        <option value="PE">PE</option>
-                                        <option value="PI">PI</option>
-                                        <option value="RJ">RJ</option>
-                                        <option value="RN">RN</option>
-                                        <option value="RS">RS</option>
-                                        <option value="RO">RO</option>
-                                        <option value="RR">RR</option>
-                                        <option value="SC">SC</option>
-                                        <option value="SP">SP</option>
-                                        <option value="SE">SE</option>
-                                        <option value="TO">TO</option>
-                                    </Input>{' '}
-                                    <Button type="submit">Buscar</Button>
-                            </InputGroup>
-                            <br/>
                             <Label>Região: </Label>{' '}
                             <Label value="regiao">
                                 {
                                     dados.map(dado => <div>{dado.regiao}</div>)
+                                }
+                            </Label>
+                            <br/>
+                            <Label>Cidade: </Label>{' '}
+                            <Label value="cidade">
+                                {
+                                    dados.map(dado => <div>{dado.cidade}</div>)
                                 }
                             </Label>
                             <br/>
@@ -99,12 +68,9 @@ class Home extends Component {
                             <Label value="mortes">{
                                     dados.map(dado => <div>{dado.obitosAcumulado}</div>)
                                 }</Label>
-
-                                
-                            
-                          
                     </CardBody>
                 </Card>
+                <Map></Map>
             </div>
         )
     }
